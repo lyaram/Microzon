08427ecaacf62e0067e643a18bc4cace9f263035
Javier Alvarez <lyaram@yahoo.es>
2011-11-12 00:09:44 +0100

    Simplificando QBOT

--- lib/pages/etsy_home_page.rb
+++ lib/pages/etsy_home_page.rb
@@ -1,9 +1,11 @@
 class EtsyHomePage < BasePageClass
 
-  expected_title "Etsy - Your place to buy and sell all things handmade, vintage, and supplies"
+#  expected_title "Etsy - Your place to buy and sell all things handmade, vintage, and supplies"
   direct_url BASE_URL
+puts "before_link"
   link :buy, :text => "Buy"
-
+puts "after_link"
+  
   def click_buy
     self.buy
   end
