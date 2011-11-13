5e5f25f9c269f543bc53767567eac56b44e66ab6
Javier Alvarez <lyaram@yahoo.es>
2011-11-13 01:07:49 +0100

    Recuperando error, devolviendo funciones originales (01)

--- lib/pages/etsy_home_page.rb
+++ lib/pages/etsy_home_page.rb
@@ -1,18 +1,18 @@
 class EtsyHomePage < BasePageClass
 
-#  expected_title "Etsy - Your place to buy and sell all things handmade, vintage, and supplies"
-#  direct_url BASE_URL
-#  link :buy, :text => "Buy"
-  direct_url "http://www.turismepropbarcelona.cat/ca/"
-  #screenshot = "./screenshots/WEBIMG_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
-  @screenshot = "./screenshots/WEBIMG_www_turismepropbarcelona_cat.png"
-  Browser::BROWSER.driver.save_screenshot(@screenshot)
-  embed @screenshot, 'image/png'
-
-  direct_url "http://www.diba.es/parcsn/parcs/home.asp"
-  @screenshot = "./screenshots/WEBIMG_www_diba_es_parcsn.png"
-  Browser::BROWSER.driver.save_screenshot(@screenshot)
-  embed @screenshot, 'image/png'
+  expected_title "Etsy - Your place to buy and sell all things handmade, vintage, and supplies"
+  direct_url BASE_URL
+  link :buy, :text => "Buy"
+#  direct_url "http://www.turismepropbarcelona.cat/ca/"
+#  #screenshot = "./screenshots/WEBIMG_#{scenario.name.gsub(' ','_').gsub(/[^0-9A-Za-z_]/, '')}.png"
+#  @screenshot = "./screenshots/WEBIMG_www_turismepropbarcelona_cat.png"
+#  Browser::BROWSER.driver.save_screenshot(@screenshot)
+#  embed @screenshot, 'image/png'
+#
+#  direct_url "http://www.diba.es/parcsn/parcs/home.asp"
+#  @screenshot = "./screenshots/WEBIMG_www_diba_es_parcsn.png"
+#  Browser::BROWSER.driver.save_screenshot(@screenshot)
+#  embed @screenshot, 'image/png'
   
 #  def click_buy
 #    self.buy
