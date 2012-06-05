9ea850dbb5217a18173d8623bf19237f541a33e9
lyaram <lyaram@yahoo.es>
2012-06-05 14:00:52 +0200

    Testing ruby 02

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -3,6 +3,6 @@ class BookingPage < BasePageClass
 #puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 03")
   direct_url "http://www.booking.com/searchresults.en-gb.html?sid=17055a451915e9872449d95075e8dd5b;dcid=1;city=-1658079"
 
-  page.td(:class => 'next').a.click
+  browser.td(:class => 'next').a.click
 
 end
