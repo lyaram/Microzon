51b490e2f6fd03afafe45d9f0359bdf32cd7bed6
lyaram <lyaram@yahoo.es>
2012-06-05 14:30:17 +0200

    Testing ruby 04

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -3,6 +3,17 @@ class BookingPage < BasePageClass
 #puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 03")
   direct_url "http://www.booking.com/searchresults.en-gb.html?sid=17055a451915e9872449d95075e8dd5b;dcid=1;city=-1658079"
 
-  @browser.td(:class => 'next').a.click
+ .link(:xpath, "//td[@class='next']/a").exist? == true
+        begin
+             @browser.link(:xpath, "//td[@class='next']/a").click
 
+          rescue => e
+              rescueHandle(e)
+          end
+        end
+ else
+     puts "FAILED! Could not find Class for Component"
+         return false
+  end
+  
 end
