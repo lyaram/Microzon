1f6acfe7d6790a42f485fddfca2033bf707b0138
lyaram <lyaram@yahoo.es>
2012-06-06 12:30:50 +0200

    Testing ruby 09

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -1,4 +1,24 @@
-class BookingPage < BasePageClass
+class BookingPage 
+
+
+  include WatirPageHelper
+
+  def initialize browser, page_metrics, visit = false
+#puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 07")
+    @browser = browser
+
+    goto if visit
+ 
+    expected_element if respond_to? :expected_element
+ 
+    @browser.with_performance {|performance| page_metrics.add_page self.class, performance } unless visit
+#puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> FIN PASO 07")
+  end
+
+  def method_missing sym, *args, &block
+#puts("CODETRACE >> #{__FILE__}:#{__LINE__}")
+    @browser.send sym, *args, &block
+  end
 
 #puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 03")
   direct_url "http://www.booking.com/searchresults.en-gb.html?sid=17055a451915e9872449d95075e8dd5b;dcid=1;city=-1658079"
