14118e5664038f82f9ab448927c9d0c3bb6bf506
lyaram <lyaram@yahoo.es>
2012-06-07 01:06:22 +0200

    Testing ruby 26

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -1,9 +1,9 @@
 Given /^QBot is running$/ do
-
+  visit BookingPage
 end
 
 When /^QBot is surfing a webpage$/ do
-  visit BookingPage
+  on BookingPage do |page| page.click_buy end
 end
 
 Then /^QBot should get a snapshot$/ do
