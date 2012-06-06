043e1568defc3748c456169d37b45f070a6295a0
lyaram <lyaram@yahoo.es>
2012-06-06 13:29:38 +0200

    Testing ruby 14

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -22,10 +22,15 @@ class BookingPage
   direct_url "http://www.booking.com/searchresults.en-gb.html?sid=17055a451915e9872449d95075e8dd5b;dcid=1;city=-1658079"
 
   direct_url "http://www.google.com"
+puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 01")  
   expected_element :text_field, :name => "q"
+puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 02")  
   expected_title "Google"
+puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 03")  
   text_field :search_box, :name => "q"
+puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 04")  
   button :search, :name => "btnG"
+puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 05")  
 
   
   
