23a1c35049501f75c2265c184d91a155a00c243f
lyaram <lyaram@yahoo.es>
2012-06-06 16:10:19 +0200

    Testing ruby 22

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -1,56 +1,12 @@
-class BookingPage 
-  include WatirPageHelper
+class MyPage < BasePageClass
 
-  def initialize browser, page_metrics, visit = false
-#puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 07")
-    @browser = browser
-
-    goto if visit
- 
-    expected_element if respond_to? :expected_element
- 
-    @browser.with_performance {|performance| page_metrics.add_page self.class, performance } unless visit
-#puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> FIN PASO 07")
-  end
-
-  def method_missing sym, *args, &block
-#puts("CODETRACE >> #{__FILE__}:#{__LINE__}")
-    @browser.send sym, *args, &block
-  end
-
-#puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 03")
-  direct_url "http://www.booking.com/searchresults.en-gb.html?sid=17055a451915e9872449d95075e8dd5b;dcid=1;city=-1658079"
-
-b = Watir::Browser.new :firefox
-page.search_box = "Watirmelon" #This method is created by WatirPageHelper
-page.search #This method is created by WatirPageHelper also
-
-  #screenshot = "./screenshots/WEBIMG_ScreenshotLibre.png"
-  #b.driver.save_screenshot(screenshot)
-  #embed screenshot, 'image/png'
-
-b.close
-
-  
-end
-
-
-class MyPage
-  include WatirPageHelper
-
-  direct_url "http://www.google.es"
+  direct_url "http://www.google.com"
   expected_element :text_field, :name => "q"
   expected_title "Google"
   text_field :search_box, :name => "q"
   button :search, :name => "btnG"
 
-  def initialize browser, visit = false
-    @browser = browser
-    goto if visit
-
-    expected_element if respond_to? :expected_element
-    has_expected_title? if respond_to? :has_expected_title?
-  end
+  self.search_box = "Watirmelon"
+  self.search
+  
 end
-
-
