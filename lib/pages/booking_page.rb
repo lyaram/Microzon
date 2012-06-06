d10599b5616d0093f9f2fe6bf47f263821ee3728
lyaram <lyaram@yahoo.es>
2012-06-07 00:50:25 +0200

    Testing ruby 24

--- lib/pages/booking_page.rb
+++ lib/pages/booking_page.rb
@@ -1,12 +1,12 @@
 class MyPage < BasePageClass
 
-  direct_url "http://www.google.com"
-  expected_element :text_field, :name => "q"
-  expected_title "Google"
-  text_field :search_box, :name => "q"
-  button :search, :name => "btnG"
+  expected_title "Etsy - Your place to buy and sell all things handmade, vintage, and supplies"
+  direct_url "www.etsy.com"
+  link :buy, :text => "Buy"
 
-  search_box = "Watirmelon"
-  search
+  def click_buy
+    self.buy
+  end
+
   
 end
