6659beedd15eae72a2896762b1e0e629d37d3d58
lyaram <lyaram@yahoo.es>
2012-07-25 13:20:48 +0200

    Ruby Testing 169

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -12,13 +12,13 @@ When /^QBot is surfing a webpage$/ do
 
 
     #idLaunch = page.getLaunch
+    #idLaunch='00000006'
 
-
-    idLaunch='00000006'
+    idLaunch = page.getLaunch
     page.launch idLaunch,'VENERE_HotelList.Buenos Aires', 'http://www.venere.com/es/argentina/buenos-aires/#p|1;f:o|venere_ranking-t|C-z|all_areas-r|r6-c|all_rate', '//*[@id="next" and @class="next"]', '//*[@id="paging_bottom"]', '//*[@id="loading" and @style!="display: none;"]'
     page.launch idLaunch,'VENERE_HotelList.Tegucigalpa', 'http://www.venere.com/es/honduras/tegucigalpa/#p|1;f:o|venere_ranking-t|C-z|-r|r6-c|all_rate', '//*[@id="next" and @class="next"]', '//*[@id="paging_bottom"]', '//*[@id="loading" and @style!="display: none;"]'
 
-    idLaunch='00000007'
+    idLaunch = page.getLaunch
     page.launch idLaunch,'TRAVELOCITY_HotelList.REPESCA01.Managua', 'http://www.travelocity.com/trips/hotellist/datelessList?ac_country=NI&city=Managua&configId=S21949757&numRooms=1&path=hotels&pTxId=3719499&guestCodes=ADULT&guestCounts=1&hotelMaxReturnPerPage=25&startIndex=0', '(//a[@class="nextLink"])[1]', '(//div[@class="products"]/div[starts-with(@class,"product")])[last()]', ''
     page.launch idLaunch,'TRAVELOCITY_HotelList.REPESCA01.Montevideo', 'http://www.travelocity.com/trips/hotellist/datelessList?ac_country=UY&city=Montevideo&configId=S21949757&numRooms=1&path=hotels&pTxId=3719321&guestCodes=ADULT&guestCounts=1&hotelMaxReturnPerPage=25&startIndex=0', '(//a[@class="nextLink"])[1]', '(//div[@class="products"]/div[starts-with(@class,"product")])[last()]', ''
     page.launch idLaunch,'TRAVELOCITY_HotelList.REPESCA01.Quito ', 'http://www.travelocity.com/trips/hotellist/datelessList?ac_country=EC&city=Quito&configId=S21949757&numRooms=1&path=hotels&pTxId=3724484&guestCodes=ADULT&guestCounts=1&hotelMaxReturnPerPage=25&startIndex=0', '(//a[@class="nextLink"])[1]', '(//div[@class="products"]/div[starts-with(@class,"product")])[last()]', ''
