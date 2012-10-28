38acf6811563400d74870076922f03b2394daba3
lyaram <lyaram@yahoo.es>
2012-10-28 15:36:48 +0100

    Ruby Testing 209

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -40,15 +40,15 @@ When /^QBot is surfing a webpage$/ do
     #idLaunch = "00000058"
     idLaunch = page.getLaunch
     
-    page.launch idLaunch,'RESTALO_RestList.CACERES', 'http://www.restalo.es/restaurantes-caceres', '//div[@id="pagerContainerList"]/ul[@id="pager"]/li[@class="last"]/a', '//div[@id="pagerContainerList"]', ''
-    page.launch idLaunch,'ATRAPALO_RestList.CACERES', 'http://www.atrapalo.com/restaurantes/caceres_d23.html', '//*[@id="resultados_restaurantes"]//a[@title="Siguiente"]', '//div[@id="resultados_restaurantes"]/div[@class="pagerResultsBox margintop10"]', ''
-    page.launch idLaunch,'SALIR_RestList.CACERES', 'http://www.salir.com/caceres/restaurantes', '//*[@id="itemList"]//a[@class="siguiente linkPaginador"]', '//div[@class="footer"]', ''
-    page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Caceres-1-11-910/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
-    page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Plasencia-1-11-942/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
-    page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Trujillo-1-11-953/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
-    page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Coria-1-11-918/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
-    page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Guadalupe-1-11-923/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
-    # page.launch idLaunch,'TRIPADVISOR_RestList.CACERES', 'http://www.tripadvisor.es/Restaurants-g2435893-Province_of_Caceres_Extremadura.html', '//*[@id="EATERY_LIST_CONTENTS"]//a[text()="»"]', '//*[@id="EATERY_LIST_CONTENTS"]', ''
+    # page.launch idLaunch,'RESTALO_RestList.CACERES', 'http://www.restalo.es/restaurantes-caceres', '//div[@id="pagerContainerList"]/ul[@id="pager"]/li[@class="last"]/a', '//div[@id="pagerContainerList"]', ''
+    # page.launch idLaunch,'ATRAPALO_RestList.CACERES', 'http://www.atrapalo.com/restaurantes/caceres_d23.html', '//*[@id="resultados_restaurantes"]//a[@title="Siguiente"]', '//div[@id="resultados_restaurantes"]/div[@class="pagerResultsBox margintop10"]', ''
+    # page.launch idLaunch,'SALIR_RestList.CACERES', 'http://www.salir.com/caceres/restaurantes', '//*[@id="itemList"]//a[@class="siguiente linkPaginador"]', '//div[@class="footer"]', ''
+    # page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Caceres-1-11-910/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
+    # page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Plasencia-1-11-942/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
+    # page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Trujillo-1-11-953/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
+    # page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Coria-1-11-918/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
+    # page.launch idLaunch,'BUSCORESTAURANTES_RestList.CACERES', 'http://www.buscorestaurantes.com/Restaurantes-en-Guadalupe-1-11-923/1-1.html', '//div[@class="block-pagination"]/a[text()="Siguiente"]', '//div[@class="block-pagination"]', ''
+    page.launch idLaunch,'TRIPADVISOR_RestList.CACERES', 'http://www.tripadvisor.es/Restaurants-g2435893-Province_of_Caceres_Extremadura.html', '//*[@id="EATERY_LIST_CONTENTS"]//a[text()="»"]', '//*[@id="EATERY_LIST_CONTENTS"]', ''
 
   end
   
