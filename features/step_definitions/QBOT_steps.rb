e62eea958a65df760c364c5fe4f48d066c0202d8
lyaram <lyaram@yahoo.es>
2013-05-30 11:52:38 +0200

    Ruby Testing 301

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -40,18 +40,7 @@ When /^QBot is surfing a webpage$/ do
     #idLaunch = "000000040"
     idLaunch = page.getLaunch
     
-    page.launch idLaunch, 'ATRAPALO_HotelList.Lleida', 'http://www.atrapalo.com/hoteles/europa/espana/lleida/' , '//*[@id="paginator"]/ul/li[text()=">"]', '//*[@id="content"]//div[@class="box box-paginacion clear"]', ''
-    page.launch idLaunch, 'BOOKING_HotelList.Lleida', 'http://www.booking.com/searchresults.es.html?order=name&rows=50&region=763', '//*[@id="ajaxsrwrap"]/div/a[starts-with(@class,"paging-next")]', '//*[@id="footernote"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST1.Lleida', 'http://www.tripadvisor.es/Hotels-g2431169-Province_of_Lleida_Catalonia-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST2.Lleida', 'http://www.tripadvisor.es/Hotels-g2431169-c2-Province_of_Lleida_Catalonia-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST3.Lleida', 'http://www.tripadvisor.es/Hotels-g2431169-c3-Province_of_Lleida_Catalonia-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST4.Lleida', 'http://www.tripadvisor.es/VacationRentals-g2431169-Reviews-Province_of_Lleida_Catalonia-Vacation_Rentals.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'ATRAPALO_HotelList.Huesca', 'http://www.atrapalo.com/hoteles/europa/espana/huesca/', '//*[@id="paginator"]/ul/li[text()=">"]', '//*[@id="content"]//div[@class="box box-paginacion clear"]', ''
-    page.launch idLaunch, 'BOOKING_HotelList.Huesca', 'http://www.booking.com/searchresults.es.html?order=name&rows=50&region=2078', '//*[@id="ajaxsrwrap"]/div/a[starts-with(@class,"paging-next")]', '//*[@id="footernote"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST1.Huesca', 'http://www.tripadvisor.es/Hotels-g1238673-Province_of_Huesca_Aragon-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST2.Huesca', 'http://www.tripadvisor.es/Hotels-g1238673-c2-Province_of_Huesca_Aragon-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST3.Huesca', 'http://www.tripadvisor.es/Hotels-g1238673-c3-Province_of_Huesca_Aragon-Hotels.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
-    page.launch idLaunch, 'TRIPADVISOR_HotelList.EST4.Huesca', 'http://www.tripadvisor.es/VacationRentals-g1238673-Reviews-Province_of_Huesca_Aragon-Vacation_Rentals.html', '//*[@id="pager_bottom"]/a[contains(@class,"pageNext")]', '//*[@id="FOOT"]', ''
+    page.launch idLaunch, 'BOOKING_HotelList.Barcelona', 'http://www.booking.com/searchresults.es.html?order=name;rows=50;city=-372490', '//*[@id="ajaxsrwrap"]/div/a[starts-with(@class,"paging-next")]', '//*[@id="footernote"]', ''
 
      
   end
