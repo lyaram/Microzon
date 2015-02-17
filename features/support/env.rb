2ab6d599bf6bbc5567c785f17d06229a5e4b9ba2
lyaram <lyaram@yahoo.es>
2015-02-17 21:52:29 +0100

    Commit 553

--- features/support/env.rb
+++ features/support/env.rb
@@ -69,7 +69,11 @@ module Browser
 
   BROWSER = Watir::Browser.new DRIVER, :profile => profile, :http_client => client
 #puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> FIN PASO 04")
-  
+  BROWSER.goto 'https://check.torproject.org/?lang=en_US'
+  puts BROWSER.divs(:class => 'content').first.text
+  BROWSER.goto 'about:blank'
+
+
   def visit page_class, &block
 #puts("CODETRACE >> #{__FILE__}:#{__LINE__} ----> PASO 05")
     on page_class, true, &block
