8d923b00c30d4d4507ab4adb119798022487fc81
lyaram <lyaram@yahoo.es>
2015-11-11 01:27:01 +0100

    Commit 582

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -463,7 +463,7 @@ class NextLinkPage
         reintentos += -1
         puts '@browser.element(:xpath,nextlink).click.Retries:' + reintentos.to_s ; $stdout.flush
         @browser.element(:xpath,nextlink).click
-        sleep 3
+        sleep 10
       rescue Exception
         puts $!, $@
         if reintentos>0
