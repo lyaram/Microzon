691c42c290a278571fbfeab15bf42e50eb0eef3e
lyaram <lyaram@yahoo.es>
2015-11-19 12:42:44 +0100

    Commit 598

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -352,7 +352,7 @@ class NextLinkPage
         end
       end
 
-      linkMore = '(/quitaestodeaqui//*[@id="REVIEWS"]/div[starts-with(@id,"review_")]//span[@class="partnerRvw"]/span[contains(@class,"moreLink") and not(ancestor-or-self::*[contains(translate(@style," ",""),"display:none")])])[1]'
+      linkMore = '(//*[@id="REVIEWS"]/div[starts-with(@id,"review_")]//span[@class="partnerRvw"]/span[contains(@class,"moreLink") and not(ancestor-or-self::*[contains(translate(@style," ",""),"display:none")])])[1]'
       reintentos = 15
       while @browser.element(:xpath,linkMore).exists?
         reintentos += -1
@@ -375,7 +375,7 @@ class NextLinkPage
 
       puts("CODETRACE >> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-      linkMore = '/quitaestodeaqui//*[@class="entry"]//span[starts-with(@class,"taLnk") and not(ancestor-or-self::*[contains(translate(@style," ",""),"display:none")])]'
+      linkMore = '//*[@class="entry"]//span[starts-with(@class,"taLnk") and not(ancestor-or-self::*[contains(translate(@style," ",""),"display:none")])]'
       reintentos = 10
       while @browser.element(:xpath,linkMore).exists?
         reintentos += -1
