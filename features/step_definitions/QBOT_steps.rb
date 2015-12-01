70f9751a89a5b7b2900e347de60a57d0107546a5
lyaram <lyaram@yahoo.es>
2015-12-01 05:45:55 +0100

    Commit 669

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -86,8 +86,8 @@ When /^QBot is surfing a webpage$/ do
       pillamientos = 0
 
       loop do
-      puts("CODETRACE >> #{__FILE__}:#{__LINE__}"); puts('SELECT min(idTarget) FROM tblTargets where Disabled=2;');$stdout.flush
-        idTarget = con.query("SELECT min(idTarget) FROM tblTargets where Disabled=2;").fetch_row.first
+      puts("CODETRACE >> #{__FILE__}:#{__LINE__}"); puts('SELECT min(idTarget) FROM tblTargets where Disabled=4;');$stdout.flush
+        idTarget = con.query("SELECT min(idTarget) FROM tblTargets where Disabled=4;").fetch_row.first
         puts "Getting idTARGET (" + Time.now.strftime("%Y-%m-%d %H:%M:%S") + ") ....... "
         puts idTarget
 
