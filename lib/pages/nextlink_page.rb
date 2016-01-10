ff7786d6f95ee1c7355c71a14e57ac15ee54c68c
lyaram <lyaram@yahoo.es>
2016-01-10 08:59:34 +0100

    Commit 716

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -356,7 +356,7 @@ class NextLinkPage
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
     
-    repeatPageCountDownMax = 20
+    repeatPageCountDownMax = 30
     repeatPageCountDown = repeatPageCountDownMax
     
     @numPag = 0
