21b05f746596d201cf1ba0980ee437d2c2a74e30
lyaram <lyaram@yahoo.es>
2016-03-31 14:13:05 +0200

    Commit 746

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -310,7 +310,13 @@ class NextLinkPage
         end
       end
  
- 
+      allLangRadioButton = './/*[@name='filterLang' and @value='ALL']'
+      if @browser.element(:xpath,allLangRadioButton).exists?
+        if @browser.element(:xpath,allLangRadioButton).visible?
+          @browser.element(:xpath,allLangRadioButton).click
+        end
+      end
+
        #Desactivando filtro activo si lo si hay
        ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
        if @browser.element(:xpath,'//button[@class="clear"]').exists?
