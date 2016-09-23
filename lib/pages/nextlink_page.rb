f3f065e4dcc48030481de3e377221063bfccaea2
lyaram <lyaram@yahoo.es>
2016-09-23 10:49:49 +0200

    Commit 755

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -364,12 +364,12 @@ class NextLinkPage
               sleep 500
               ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
               chkdfilterpath = ".//li/span/input[@name='filterSegment' and @checked and @value='#{filternumber}']"
-              begin
-                @browser.element(:xpath,chkdfilterpath).wait_until_present
-              rescue
-                puts("Filter sin clicar...")
-                ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-              end
+              #begin
+              #  @browser.element(:xpath,chkdfilterpath).wait_until_present
+              #rescue
+              #  puts("Filter sin clicar...")
+              #  ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+              #end
               if @browser.element(:xpath,chkdfilterpath).exists?
                 retries = 0
               end
