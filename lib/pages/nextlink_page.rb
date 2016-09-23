4fba397caf91b87540534082e7a7299b26469152
lyaram <lyaram@yahoo.es>
2016-09-23 22:58:58 +0200

    Commit 759

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -367,6 +367,8 @@ class NextLinkPage
            retries = 10
            while retries > 0
               retries += -1
+              puts("Ventanas: #{b.windows.count}")
+              puts("Titulo:   #{b.title}")
               ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
               @browser.element(:xpath,filterpath).click
               sleep 500
