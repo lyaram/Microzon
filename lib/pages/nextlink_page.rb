029094e4032bc36d3cf81fd0951888cfb9423a49
lyaram <lyaram@yahoo.es>
2016-10-02 01:19:15 +0200

    Commit 782

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -483,7 +483,7 @@ class NextLinkPage
     if descripcion.include? 'GMapsPlace.ID' 
       puts "In GMapsPlace"
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-      @browser.element(:xpath,"//*[@class='section-write-review-container']").click
+      @browser.element(:xpath,"//*[@jsaction='pane.rating.moreReviews']").click
       sleep 1
       puts "waiting reviews"
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
