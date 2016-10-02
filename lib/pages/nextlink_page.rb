af1f47a03e26359ba4daba58cd5d0294ae16a00d
lyaram <lyaram@yahoo.es>
2016-10-02 23:36:46 +0200

    Commit 797

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -485,7 +485,7 @@ class NextLinkPage
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
       enlaceReviews = "//*[contains(@jsaction,'reviewChart') and not(contains(translate(@style,' ',''),'display:none'))]"
       hayReviews = true
-      puts @browser.element(:xpath,"//*[contains(text(),'REVIEWS')]").outer_html
+      puts @browser.element(:xpath,"//button[contains(text(),'review')]").outer_html
       begin
         puts "esperando enlaceReviews"    ; $stdout.flush
         @browser.element(:xpath,enlaceReviews).wait_until_present
