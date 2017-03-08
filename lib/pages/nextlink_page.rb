56dcbe0e2a0eabd12728e8a5f50b32ea9b38a119
lyaram <lyaram@yahoo.es>
2017-03-08 20:04:20 +0100

    Commit 895

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -224,7 +224,7 @@ class NextLinkPage
 
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-      if url.include? 'ShowUserReviews-' && !(descripcion.include?('.IDRev'))
+      if url.include?('ShowUserReviews-') && not(descripcion.include?('.IDRev'))
         puts 'en showusersreviews'; $stdout.flush
           while @browser.element(:xpath,'//*[@id="REVIEWS"]//div[@class="pageNumbers"]/a[@data-page-number="1"]').exists?
             puts 'go click page 1'; $stdout.flush
