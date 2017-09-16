d78515f847423ae863ecd52e4a024b946ae8fcd2
lyaram <lyaram@yahoo.es>
2017-09-16 12:10:42 +0200

    Commit 918

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -835,7 +835,7 @@ class NextLinkPage
       ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
       closeModal = '//*[@class="ui_close_x" and not(ancestor-or-self::*[contains(@class,"hidden")])]'
-      if @browser.element(:xpath,closeModal).exists?
+      if @browser.element(:xpath,closeModal).visible?
         @browser.element(:xpath,closeModal).click
       end
 
