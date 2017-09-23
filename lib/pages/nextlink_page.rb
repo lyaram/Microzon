46244d2c4b000567c50dfa8ea3e145d0ef9ab984
lyaram <lyaram@yahoo.es>
2017-09-23 13:27:03 +0200

    Commit 926

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -459,8 +459,6 @@ class NextLinkPage
 
           ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-          @browser.element(:xpath,"//button[@class='clear']").wait_until_present
-          ahora = Time.now; tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
         rescue => error
           puts("ERROR:")
           puts("Error.class: #{error.class}")
