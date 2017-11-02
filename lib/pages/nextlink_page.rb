37877c77ed0bd497c833f91f84d3b12dd4f22f29
lyaram <lyaram@yahoo.es>
2017-11-02 23:23:57 +0100

    Commit 962

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -628,6 +628,8 @@ class NextLinkPage
 
       ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
+      archivandoTraza
+      
       boton = @browser.element(:xpath,enlaceReviews)
       @browser.execute_script('arguments[0].scrollIntoView();', boton)
       boton.wd.location_once_scrolled_into_view
@@ -640,6 +642,7 @@ class NextLinkPage
         clickboton = 5
         while clickboton > 0
           begin
+            archivandoTraza
             puts "clicking enlaceReviews"    ; $stdout.flush
             @browser.element(:xpath,enlaceReviews).click
             sleep 5
