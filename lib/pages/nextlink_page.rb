9ad15bf0dc68e672264dfa5268cbc0c78fb8179a
lyaram <lyaram@yahoo.es>
2018-10-26 03:26:21 +0200

    Commit 1038

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -1334,7 +1334,7 @@ class NextLinkPage
       #Tratamiento KeepOnPaging especial para capturas de fichas con muchas paginas que suelen caer en mitad de proceso.
       if descripcion.include?('.KeepOnPaging.')
         if @browser.element(:xpath,nextlink).exists?
-          urlNextLink = @browser.element(:xpath,nextlink + '/@href').text.strip
+          urlNextLink = @browser.element(:xpath,nextlink).attribute_value('href').text.strip
           unless urlNextLink.include? 'www.tripadvisor.' 
             urlNextLink = 'https://www.tripadvisor.es#{urlNextLink}'
           end
