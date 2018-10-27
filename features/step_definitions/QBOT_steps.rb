f874314823bc627666ea50506a17ebb2ecffcd03
lyaram <lyaram@yahoo.es>
2018-10-27 18:56:00 +0200

    Commit 1050

--- features/step_definitions/QBOT_steps.rb
+++ features/step_definitions/QBOT_steps.rb
@@ -147,8 +147,9 @@ When /^QBot is surfing a webpage$/ do
           #Tratamiento KeepOnPaging especial para capturas de fichas con muchas paginas que suelen caer en mitad de proceso.
           if description.include?('.KeepOnPaging.')
             if con.query("select count(*) from tblKeepOnPaging WHERE idTarget = #{idTarget} AND NOT(updatetime is null);").fetch_row.first.to_i == 0
-              con.query("DELETE FROM tblKeepOnPaging WHERE idTarget = #{idTarget};")
-              con.query("INSERT tblKeepOnPaging (idTarget) VALUES (#{idTarget});")
+              con.query("DELETE FROM tblKeepOnPaging WHERE idTarget = #{idTarget};")          
+              inserttime = Time.now.strftime("%Y-%m-%d %H:%M:%S")  #vigilar que no haya que meterlo en utc Time.now.utc.to_s(:db)
+              con.query("INSERT tblKeepOnPaging (idTarget, time_stamp) VALUES (#{idTarget}, #{inserttime});")
             else
               url = con.query("select nextLink from tblKeepOnPaging WHERE idTarget = #{idTarget};").fetch_row.first
               $keep_on_paging_in_progress = true
