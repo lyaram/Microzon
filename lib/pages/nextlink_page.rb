979755f380def14694de0af4fd366f9c493aa2ee
lyaram <lyaram@yahoo.es>
2018-10-28 07:46:06 +0100

    Commit 1053

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -1047,7 +1047,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; 
   
           ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
         end
-      rescue
+      rescue error
         puts("ERROR:")
         puts("Error.class: #{error.class}")
         puts("Error.msg: #{error.message}")
