3842d87ba9a2c6e25f24982108bff618431a86ad
lyaram <lyaram@yahoo.es>
2018-11-05 05:49:34 +0100

    Commit 1075

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -1466,13 +1466,13 @@ class NextLinkPage
           
       ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-          if descripcion.include? 'GMapsPlaceSearchLocalReviews.'
-            nextpageurl = @browser.element(:xpath,nextlink).attribute_value('href')
-            @browser.goto nextpageurl
-            @browser.element(:xpath,checkPageCompleted).wait_until_present
-          else
+#          if descripcion.include? 'GMapsPlaceSearchLocalReviews.'
+#            nextpageurl = @browser.element(:xpath,nextlink).attribute_value('href')
+#            @browser.goto nextpageurl
+#            @browser.element(:xpath,checkPageCompleted).wait_until_present
+#          else
             @browser.element(:xpath,nextlink).click
-          end
+#          end
           sleep 2
         rescue Exception
           puts $!, $@
