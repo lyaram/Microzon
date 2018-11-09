c49bdb4b25655ebb2d741c940cd2151996a67389
lyaram <lyaram@yahoo.es>
2018-11-09 15:31:58 +0100

    Commit 1077

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -503,6 +503,8 @@ class NextLinkPage
         puts 'timeout esperando filtro'
        end
 
+archivandoTraza
+
       ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
        if @browser.element(:xpath,"//*[@data-param='filterSegment']").exists?
         filternumber = descripcion[/.*\.TAFilterSegment_(.*)\./,1]
