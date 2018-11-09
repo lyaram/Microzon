744b7d927f8ef61ec1575abd9cb1c31a5c78f2c7
lyaram <lyaram@yahoo.es>
2018-11-09 15:36:45 +0100

    Commit 1078

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -528,7 +528,7 @@ archivandoTraza
           fe.send_keys :arrow_up
 
           ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-          @browser.element(:xpath,"#{filterpath}/input").click
+          @browser.element(:xpath,"#{filterpath}/label").click
           reintentos = 5
           begin
             reintentos += -1
