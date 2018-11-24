ff9acc72680ac93fe4d55630c6da6509460588ee
lyaram <lyaram@yahoo.es>
2018-11-24 01:25:41 +0100

    Commit 1111

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -847,7 +847,7 @@ class NextnkPage
     loop do
       @numPag += 1
 
-      puts("BROWSER URL: {@browser.url}")
+      puts("BROWSER URL: #{@browser.url}")
             
       #archivandoTraza; 
           ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
