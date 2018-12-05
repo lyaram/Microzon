17dba09886055e6465fbf702be7023fe592aab8b
lyaram <lyaram@yahoo.es>
2018-12-05 05:54:54 +0100

    Commit 1117

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -1454,6 +1454,10 @@ class NextLinkPage
           ignore_exception { reviewInfoTag08 = con.quote(node.element(:xpath,".//ul[@class='review_item_info_tags']/li[8]").text) }
           ignore_exception { reviewInfoTag08 = con.quote(node.element(:xpath,".//ul[@class='review_item_info_tags']/li[8]").text) }
           ignore_exception { reviewInfoTag08 = con.quote(node.element(:xpath,".//ul[@class='review_item_info_tags']/li[8]").text) }
+  
+          puts("reviewerName: #{reviewerName}")
+          puts("quote: #{quote}")
+          ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
           
           sqlInsert = "INSERT INTO `Navigator`.`tblBKReviewsIndiv` (IdBKReviewsFicha, Posicion, reviewerName, nacionality, score, quote, reviewDate, " +
                                                                    "reviewInfoTag01, reviewInfoTag02, reviewInfoTag03, reviewInfoTag04," +
