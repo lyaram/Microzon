ebc44c70ef8730f0c9ddd36b4d3316966b50231e
lyaram <lyaram@yahoo.es>
2019-03-06 00:26:17 +0100

    Commit 1060

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -2050,6 +2050,11 @@ def launchDataTA con, idTarget, idConexion, idLaunch, descripcion, url, nextlink
 
     lasttime = Time.now.to_f
 
+puts "idLaunch: #{idLaunch}"
+puts "descripcion: #{descripcion}"
+puts "url: #{url}"
+puts "nextlink: #{nextlink}"
+puts "checkPageCompleted: #{checkPageCompleted}"
     ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
     idCaptura = prepararCaptura idLaunch, descripcion, url, nextlink, checkPageCompleted 
