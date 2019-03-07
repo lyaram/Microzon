313fdb293fc87892fded7411071f4e7f90999354
lyaram <lyaram@yahoo.es>
2019-03-07 07:44:36 +0100

    Commit 1077

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -116,8 +116,8 @@ def launchDataTA con, idTarget, idConexion, idLaunch, descripcion, url, nextlink
 
     ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-  uid = html.scan(/"uid":"(.[^"]*)"/).first.first
-  rid = html.scan(/data-reviewid="(.[^"]*)"/).first.first
+  uid = html.scan(/"uid":"(.[^"]*)"/i).first.first
+  rid = html.scan(/data-reviewid="(.[^"]*)"/i).first.first
 
 
 
