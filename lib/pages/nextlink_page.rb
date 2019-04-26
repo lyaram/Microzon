4b9744578cdd97ccbc0720e12ecde850558b79cf
lyaram <lyaram@yahoo.es>
2019-04-26 10:46:52 +0200

    Commit 1090

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -1857,7 +1857,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - lasttime; lasttime = ahora.to_f; 
     # sudo rsync -a /var/lib/jenkins/BotStoring/png /vol/BotStoring/
   end
   
-  def prepararCaptura con, idTarget, idConexion, idLaunch, idCaptura, page, descripcion, urlOrig
+  def prepararCaptura idLaunch, descripcion, url, nextlink, checkPageCompleted
 
     #folderbase = Dir.home() + "/BotStoring"
     folderlaunches = "/volHTML/" #"Dir.home() + "/BotStoring/launches/" 
