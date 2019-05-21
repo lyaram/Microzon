31f99b2e7252b36c02a5006eebe23d2f7b5feb78
lyaram <lyaram@yahoo.es>
2019-05-21 15:38:09 +0200

    Commit 1060

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -4207,7 +4207,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
       ignore_exception { cert2 = con.quote(node.element(:xpath,"(//*[contains(@class,'certificate')])[1]").text) }
       ignore_exception { cert3 = con.quote(node.element(:xpath,"(//*[contains(@class,'excellence')])[1]").text) }
 
-      ignore_exception { securityReviews = con.quote(node.element(:xpath,"//*[contains(@for,'HealthAndSafetyFilter')]").text) }
+      ignore_exception { securityReviews = con.quote(node.element(:xpath,"//*[contains(@for,'Safety')]").text) }
 
 
     
