edaa643d654382c36aaf13d66b78b4bdfb329f9c
lyaram <lyaram@yahoo.es>
2019-12-19 15:47:17 +0100

    Commit 1140

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -4257,7 +4257,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
 
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-           tjson.delete! '\\'
+           tjson = JSON.parse(tjson)
 
 puts(tjson);
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
