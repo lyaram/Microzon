69a65ef2913c453da8eabf280439a186d5f686f6
lyaram <lyaram@yahoo.es>
2020-03-22 20:07:58 +0100

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -988,6 +988,8 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
       puts "respuesta:"
       puts(respuesta)
       htmlpage = "<style>" + respuesta.scan(/[^>];\<style\>(.*)\<\/div\>4;[^<]/).first.first + "</div>"
+      puts "htmlpage:"
+      puts(htmlpage)
       retHttp = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body>" + htmlpage + "</body></html>"
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
