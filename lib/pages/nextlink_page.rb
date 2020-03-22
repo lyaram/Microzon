c181758b27618bc8e57e209ffbc54918af1a5a7a
lyaram <lyaram@yahoo.es>
2020-03-22 19:04:40 +0100

    Commit 1160

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -968,8 +968,8 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
 #      request.add_field('X-Requested-With' , 'XMLHttpRequest' )
 #      request.add_field('DNT' , '1')
 #      request.add_field('Accept-Language' , 'es-ES,es;q=0.8,en-US;q=0.5,en;q=0.3')
-      request.add_field('Accept' , 'text/html, */*')
-      request.add_field('Content-Type' , 'application/x-www-form-urlencoded; charset=utf-8' )
+#      request.add_field('Accept' , 'text/html, */*')
+#      request.add_field('Content-Type' , 'application/x-www-form-urlencoded; charset=utf-8' )
       request.add_field('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko' )
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
       response = http.request(request)
