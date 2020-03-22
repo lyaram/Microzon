6b3fbcd4177ae10ce92c53e7728514315fb477f0
lyaram <lyaram@yahoo.es>
2020-03-22 18:47:30 +0100

    Commit 1158

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -979,11 +979,12 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
       retHttp = "<!DOCTYPE html><html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"/></head><body>" + htmlpage + "</body></html>"
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-#    File.open("/tmp/httppage.htm", 'w:UTF-8') { |file| file.write(retHttp) }
-#      @browser.goto 'file:///tmp/httppage.htm'      
-#    end
-    file = File.new "/tmp/httppage.htm", + filename, 'wb+'
-    file.write Base64.decode64( retHttp )
+      file = File.new "/tmp/httppage.htm", + filename, 'wb+'
+      file.write Base64.decode64( retHttp )
+
+#      File.open("/tmp/httppage.htm", 'w:UTF-8') { |file| file.write(retHttp) }
+      @browser.goto 'file:///tmp/httppage.htm'      
+    end
     
     if descripcion.include? 'GMapsPlaceFull.' 
       puts "In GMapsPlace"
