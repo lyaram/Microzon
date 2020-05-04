2b77c6fe832883c7d2946d3071ee7131a660dec7
lyaram <lyaram@yahoo.es>
2020-05-04 09:59:45 +0200

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -978,7 +978,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
 
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-    if descripcion.include? 'GMapsReviewBox.' &&  !(url.include? 'file:///')
+    if (descripcion.include? 'GMapsReviewBox.') &&  !(url.include? 'file:///')
       fid = descripcion.scan(/\.FID([^.]*)\./).first.first
       startpage = descripcion.scan(/\.PAG([^.]*)\./).first.first
       if startpage=='0'
