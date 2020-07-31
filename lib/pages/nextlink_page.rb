0038000955e5edd10db060b6774cf1a1b05e92f3
lyaram <lyaram@yahoo.es>
2020-07-31 23:48:08 +0200

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -258,60 +258,65 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
 
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
 
-        nombreUsuario = ''
-        fechaPantalla = ''
-        texto = ''
-        nota = ''
-        userid = ''
-        lang = ''
-        langtxt = ''
-        timestamp1 = ''
-        timestamp2 = ''
-        respuestaTxt = ''
-        respuestaFechaPantalla = ''
-        respuestaTimestamp1 = ''
-        respuestaTimestamp2 = ''
-        respuestaLang = ''
-        
-        
-        ignore_exception { nombreUsuario = ojson[2][page - 1][0][1] }
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-        ignore_exception { fechaPantalla = ojson[2][page - 1][1] }
-        ignore_exception { texto = ojson[2][page - 1][3] }
-        ignore_exception { nota = ojson[2][page - 1][4] }
-        ignore_exception { userid = ojson[2][page - 1][6] }
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-        ignore_exception { lang = ojson[2][page - 1][32] }
-        ignore_exception { langtxt = ojson[2][page - 1][51] }
-        ignore_exception { timestamp1 = ojson[2][page - 1][27] }
-        ignore_exception { timestamp2 = ojson[2][page - 1][57] }
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-        ignore_exception { respuestaTxt = ojson[2][page - 1][9][1] }
-        ignore_exception { respuestaFechaPantalla = ojson[2][page - 1][9][0] }
-        ignore_exception { respuestaTimestamp1 = ojson[2][page - 1][9][3] }
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-        ignore_exception { respuestaTimestamp2 = ojson[2][page - 1][9][4] }
-        ignore_exception { respuestaLang = ojson[2][page - 1][9][7] }
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-
-        puts ""
-        puts "nombreUsuario: #{nombreUsuario}"
-        puts "fechaPantalla: #{fechaPantalla}"
-        puts "texto: #{texto}"
-        puts "nota: #{nota}"
-        puts "userid: #{userid}"
-        puts "lang: #{lang}"
-        puts "langtxt: #{langtxt}"
-        puts "timestamp1: #{timestamp1}"
-        puts "timestamp2: #{timestamp2}"
-        puts "respuestaTxt: #{respuestaTxt}"
-        puts "respuestaFechaPantalla: #{respuestaFechaPantalla}"
-        puts "respuestaTimestamp1: #{respuestaTimestamp1}"
-        puts "respuestaTimestamp2: #{respuestaTimestamp2}"
-        puts "respuestaLang: #{respuestaLang}"
-        puts ""
-        puts ""
-ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+        itemReviews = ojson[2]
+        for pos in 0..(itemReviews - 1) do
+              
+            nombreUsuario = ''
+            fechaPantalla = ''
+            texto = ''
+            nota = ''
+            userid = ''
+            lang = ''
+            langtxt = ''
+            timestamp1 = ''
+            timestamp2 = ''
+            respuestaTxt = ''
+            respuestaFechaPantalla = ''
+            respuestaTimestamp1 = ''
+            respuestaTimestamp2 = ''
+            respuestaLang = ''
+            
+            
+            ignore_exception { nombreUsuario = itemReviews[pos][0][1] }
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+            ignore_exception { fechaPantalla = itemReviews[pos][1] }
+            ignore_exception { texto = itemReviews[pos][3] }
+            ignore_exception { nota = itemReviews[pos][4] }
+            ignore_exception { userid = itemReviews[pos][6] }
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+            ignore_exception { lang = itemReviews[pos][32] }
+            ignore_exception { langtxt = itemReviews[pos][51] }
+            ignore_exception { timestamp1 = itemReviews[pos][27] }
+            ignore_exception { timestamp2 = itemReviews[pos][57] }
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+            ignore_exception { respuestaTxt = itemReviews[pos][9][1] }
+            ignore_exception { respuestaFechaPantalla = itemReviews[pos][9][0] }
+            ignore_exception { respuestaTimestamp1 = itemReviews[pos][9][3] }
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+            ignore_exception { respuestaTimestamp2 = itemReviews[pos][9][4] }
+            ignore_exception { respuestaLang = itemReviews[pos][9][7] }
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+    
+            puts ""
+            puts "pos: #{pos}"
+            puts "nombreUsuario: #{nombreUsuario}"
+            puts "fechaPantalla: #{fechaPantalla}"
+            puts "texto: #{texto}"
+            puts "nota: #{nota}"
+            puts "userid: #{userid}"
+            puts "lang: #{lang}"
+            puts "langtxt: #{langtxt}"
+            puts "timestamp1: #{timestamp1}"
+            puts "timestamp2: #{timestamp2}"
+            puts "respuestaTxt: #{respuestaTxt}"
+            puts "respuestaFechaPantalla: #{respuestaFechaPantalla}"
+            puts "respuestaTimestamp1: #{respuestaTimestamp1}"
+            puts "respuestaTimestamp2: #{respuestaTimestamp2}"
+            puts "respuestaLang: #{respuestaLang}"
+            puts ""
+            puts ""
+    ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
+        end
 
     end
     
