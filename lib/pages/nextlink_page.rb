d1a3d9c99f895a1f3456d47ac7eb9225f8fba97a
lyaram <lyaram@yahoo.es>
2020-08-01 02:18:09 +0200

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -261,7 +261,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
         itemReviews = ojson[0] #2
         puts itemReviews.to_s
   ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
-        if(itemReviews.to_s=="null")
+        if(itemReviews == {})
           puts "detectado nulo"
  ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
         end
