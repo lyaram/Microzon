3c9ee5d5da85db243e1247e220fdfe05f7842d4d
lyaram <lyaram@yahoo.es>
2020-08-01 17:22:18 +0200

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -292,7 +292,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
             fechaPantalla = ''
             texto = ''
             nota = ''
-            userid = ''
+            userId = ''
             lang = ''
             langtxt = ''
             timestamp1 = ''
@@ -308,7 +308,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
             ignore_exception { fechaPantalla = itemReviews[pos - 1][1] }
             ignore_exception { texto = itemReviews[pos - 1][3] }
             ignore_exception { nota = itemReviews[pos - 1][4] }
-            ignore_exception { userid = itemReviews[pos - 1][6] }
+            ignore_exception { userId = itemReviews[pos - 1][6] }
             ignore_exception { lang = itemReviews[pos - 1][32] }
             ignore_exception { langtxt = itemReviews[pos - 1][51] }
             ignore_exception { timestamp1 = itemReviews[pos - 1][27] }
@@ -326,7 +326,7 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
             puts "fechaPantalla: #{fechaPantalla}"
             puts "texto: #{texto}"
             puts "nota: #{nota}"
-            puts "userid: #{userid}"
+            puts "userId: #{userId}"
             puts "lang: #{lang}"
             puts "langtxt: #{langtxt}"
             puts "timestamp1: #{timestamp1}"
