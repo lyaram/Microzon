586f3d816dc0c0d6174733273e5052a11ad32121
lyaram <lyaram@yahoo.es>
2021-07-05 12:23:49 +0200

    Update nextlink_page.rb

--- lib/pages/nextlink_page.rb
+++ lib/pages/nextlink_page.rb
@@ -886,7 +886,8 @@ ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f
         @browser.element(:xpath,checkPageCompleted).wait_until_present
       rescue
         archivandoTraza;
-        if(titulo=='Access Denied')
+        puts("Titulo:   #{@browser.title}")
+        if(@browser.title=='Access Denied')
 ahora = Time.now;  tiempopasado = ahora.to_f - @lasttime; @lasttime = ahora.to_f; puts("CODETRACE (#{ahora}, +#{(tiempopasado * 1000).to_i}ms)>> #{__FILE__}:#{__LINE__}"); $stdout.flush
           @browser.goto url
         end
